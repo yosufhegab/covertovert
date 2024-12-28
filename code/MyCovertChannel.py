742648497f12a48478633cc185f7e3f139e4959d
yosufhegab <105945770+yosufhegab@users.noreply.github.com>
2024-12-28 23:27:27 +0300

    Final implementation of covert channel + README.md

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -6,74 +6,92 @@ class MyCovertChannel(CovertChannelBase):
     - You are not allowed to change the file name and class name.
     - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, the covert channel will be triggered by calling these functions.
     """
-    def __init__(self):
+    def _init_(self):
         """
-        - You can edit __init__.
+        - You can edit _init_.
         """
-        self.receiver_ip = "172.17.0.3"
         pass
 
-    def encode(self, bit):
-        return int(bit) ^ 1
-    
-    def decode(self, bit):
-        return int(bit) ^ 1
-
-    def send(self, log_file_name, parameter1, parameter2):
+    def send(self, log_file_name, receiverIp, pattern00, pattern01, pattern10, pattern11):
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        - A random binary message is generated 
+        - Two bits are extracted at a time and the possible combinations are checked
+        - Depending on what the two bits are a pattern is chosen from the patterns in the parameters
+        - Iterate through this pattern (A list of bits) and send each bit to the receiver
         """
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
-        for bit in binary_message:
-            encoded_bit = self.encode(bit)
-            
-            #dns_query = IP(dst="receiver") / UDP(dport=53) / DNS(qd=DNSQR(qname="testtest.com"), z = encoded_bit)
-            #flags = dns_query[DNS].flags
-            #flags &= 0b1111000111111111
-            #dns_query.show()
-            dns_query = IP(dst=self.receiver_ip) / UDP(dport=53) / DNS(z=encoded_bit)
-            super().send(dns_query)
+        # binary_message = self.generate_random_binary_message_with_logging(log_file_name, min_length = 16, max_length = 16) # For time testing
+
+        # import time 
+        # startTime = time.time() # Start timer
+
+        for i in range(0, len(binary_message), 2):
+            twoBits = binary_message[i: i+2]
+            if twoBits == '00':
+                pattern = pattern00
+            elif twoBits == '01':
+                pattern = pattern01
+            elif twoBits == '10':
+                pattern = pattern10
+            elif twoBits == '11':
+                pattern = pattern11
+
+            for zVal in pattern:
+                dns_query = IP(dst=receiverIp) / UDP(dport=53) / DNS(z=zVal)
+                super().send(dns_query)
+
+        # endTime = time.time() # Stop timer
+        # elapsedTime = endTime - startTime # Calculate elapsed time
+        # capacity = 128 / elapsedTime # Calculate capacity in bits/second
+        # print(f"Covert channel capacity: {capacity:.2f} bits/second") # Display capacity
         
-    def receive(self, parameter1, parameter2, parameter3, log_file_name):
+    def receive(self, log_file_name, pattern00, pattern01, pattern10, pattern11, should_stop):
         """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        - Receive the bits from sender and once all the bits in the chosen pattern is sent check which pattern it was
+        - Add the two bits corresponding to the pattern to the 'received_message' variable
+        - Once 'received_message' is a byte long add it to the 'ascii_message' variable
+        - When the end of the message is reached, make the 'should_stop' flag true and pass it into the sniff function
+        - Finally, log the full 'ascii_message'
         """
         collected_bits = ""
-        should_stop = False
         received_message = ""
+        pattern_length = len(pattern00)
+        ascii_message = ""
 
         def stop_message(packet):
             return should_stop
             
 
         def receive_packet(packet):
-            nonlocal collected_bits, should_stop, received_message
+            nonlocal collected_bits, should_stop, received_message, ascii_message
             if DNS in packet and hasattr(packet[DNS], 'z'):
 
                 dns_packet = packet[DNS]
                 z_val = dns_packet.z
 
-                decoded_bit = self.decode(z_val)
+                collected_bits += str(z_val)
+
+                if len(collected_bits) >= pattern_length:
+                    extractedPattern= collected_bits[:pattern_length]
+                    collected_bits = collected_bits[pattern_length:]
+
+                    if extractedPattern == ''.join(map(str, pattern00)):
+                        received_message += "00"
+                    elif extractedPattern == ''.join(map(str, pattern01)):
+                        received_message += "01"
+                    elif extractedPattern == ''.join(map(str, pattern10)):
+                        received_message += "10"
+                    elif extractedPattern == ''.join(map(str, pattern11)):
+                        received_message += "11"
 
 
-                collected_bits += str(decoded_bit)
-                
-                while len(collected_bits) >= 8:
-                    byte = collected_bits[:8]
-                    collected_bits = collected_bits[8:] 
-                    character = chr(int(byte, 2))
-                    print(f"received character: {character}")
-                    received_message += character
+                    if len(received_message) >= 8:
+                        char = self.convert_eight_bits_to_character(received_message[-8:])
+                        ascii_message += char
+                        received_message = ""
 
+                        if char == '.':
+                            should_stop = True
 
-                    if character == '.':
-                        should_stop =True
-        sniff(prn=receive_packet, stop_filter=stop_message, filter="udp port 53")
-                                            
-        self.log_message(received_message, log_file_name)
-
-
-if __name__ == "__main__":
-    covert_channel = MyCovertChannel()
+        sniff(prn=receive_packet, stop_filter=stop_message, filter="udp port 53")                                  
+        self.log_message(ascii_message, log_file_name)
--- code/README.md
+++ code/README.md
@@ -1,3 +1,78 @@
-# Replace with Your Covert Channel Name
+# Covert Storage Channel that exploits Protocol Field Manipulation using Z Flag field in DNS [Code: CSC-PSV-DNS-ZF]
 
-Explain your study in detail as you share your work with the community in a public repository. Anyone should understand your project when read it without having a previous information about the homework.
+
+## Explanation
+This covert channel makes use of the DNS's Z flag, a 3 bit long field reserved for future use and is always set to 0 in standard DNS implementations. Our covert channel implementation can be split up into two parts, namely the sender and the receiver functions, which are explained below:
+
+### Sender
+
+- The sender takes several parameters:
+    - log_file_name: The name of the sender log file (where the message sent will be displayed)
+
+    - receiverIp: The receiver's IP address
+
+    - pattern00: A list of bits of length 3 used to encode '00' bits
+    
+    - pattern01: A list of bits of length 3 used to encode '01' bits
+    
+    - pattern10: A list of bits of length 3 used to encode '10' bits
+    
+    - pattern11: A list of bits of length 3 used to encode '11' bits
+
+- A random binary message is generated using the 'generate_random_binary_message_with_logging' method from the 'CovertChannelBase' class.
+
+- Iterate through the binary message with a step size of 2 and read two bits at a time.
+
+- Check what the two bit values are and choose the pattern depending on this. E.g. if they are '00' then we will be using the pattern00 list to encode the bits.
+
+- Iterate through the pattern list and, for every bit in it, fill the z flag with the current bit. Send this bit through port 53 and using UDP (User Datagram Protocol), the standard port and method for DNS services, to the receiver by using the 'receiverIp' argument as the destination. This essentially sends the contents of the pattern list one bit at a time to the receiver.
+
+
+
+### Receiver
+
+- The receiver function takes the same arguments as the sender function except for the receiver ip. In addition, it takes the 'should_stop' parameter, a flag initialised to false that dictates whether we should stop receiving packets.
+
+- The receiver extraxts the bit that was put in the DNS's z flag and concatenates it to an initially empty string 'collected_bits'.
+
+- This process is repeated until the length of 'collected_bits' is greater than or equal to the pattern lists' length. (Basically until the full pattern is transmitted)
+
+- The bits are then extracted from 'collected_bits' and compared to each pattern.
+
+- Once the pattern is identified, the corresponding two bits are concatenated into 'received_message', an initially emmpty string. (E.g. If the sent pattern was pattern00 then "00" will be added to 'received_mesage')
+
+- This process is repeated until 'received_message' has a lebgth of at least 8. (This is when the received message becomes one byte long, which means it can be cinverted into an ASCII character)
+
+- The length of 'received_message' is checked to see whether it is greater than or equal to 8. If so, it is converted into an ASCII character and concatenated into the initially empty string, 'ascii_message'.
+
+- This process is repeated until the '.' character is encountered, in which case we set 'should_stop' to True.
+
+- We use 'sniff(prn=receive_packet, stop_filter=stop_message, filter="udp port 53")' to check for DNS packets and capture them when they have been transmitted by the sender.
+
+    - The prn argument specifies that for every packet that is capturedm the function 'receive_packet' should be called;
+    
+    - The stop_filter argument provides us with the option of stopping the sniffing process. When it is True it stops further packet capture
+
+    - The filter argument, in our case it is set to 'udp port 53', ensures that only DNS packets are being used to carry the data.
+
+- Finally, the message is logged using the 'log_message' function with 'ascii_message' and 'log_file_name' passed as arguments.
+
+
+### Constraints
+
+- receiverIp: This should be set to the receiver's IP.
+
+- The pattern variables in the sender function should be equal to their receiver function counterparts. (E.g. pattern00 in sender should be exactly the same as pattern00 in receiver)
+
+- All patterns should have the same length.
+
+- The patterns work for any size of 2 or more; however, we recommend using 3 because as the size gets larger the covert channel's capacity decreases. The other case of making it have a size of 2 would result in an encoding that is a bit too simple.
+
+- should_stop: This should be set to false.
+
+
+### Capacity
+
+- The code that tests the covert channel's capacity is commented out.
+
+- The covert channel's capacity was tested and resulted in a capacity of anywhere between 29 and 30 bits/second.
